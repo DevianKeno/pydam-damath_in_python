42e692ca115ce2fa66cc5fd2a215b8c805f2b25c
szy-kenn <johnkennethvelano@gmail.com>
2022-12-09 12:57:08 +0800

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -2,8 +2,10 @@ import pygame, sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption('Damath')
+reso = pygame.display.Info() # gets the video display information object
+
+screen = pygame.display.set_mode((reso.current_w/2, reso.current_h/2))
+pygame.display.set_caption('Damath') # window caption
 
 while True:
     for event in pygame.event.get():
