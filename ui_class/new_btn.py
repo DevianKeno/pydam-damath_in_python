7fa9388c9a5213d671b513a26329dae11af554f0
szy-kenn <johnkennethvelano@gmail.com>
2022-12-31 14:59:17 +0800

    added color transition in buttons

--- ui_class/new_btn.py
+++ ui_class/new_btn.py
@@ -14,9 +14,9 @@ class NButton:
                     toggled_color=(243, 112, 72), text_color=(255, 255, 255), 
                     shadow_rect_color=(38, 73, 89), shadow_hovered_color=(54, 103, 126),
                     shadow_selected_color=(54, 103, 126), shadow_disabled_color=(10, 10, 10),
-                    shadow_toggled_color=(149, 49, 30), fontsize: int = 0, 
-                    fontstyle = 'font\CookieRun_Regular.ttf', shadow_offset: int=0, 
-                    target: Callable = None, args: Iterable=[]): 
+                    shadow_toggled_color=(149, 49, 30), transition_speed = 15,
+                    fontsize: int = 0, fontstyle = 'font\CookieRun_Regular.ttf', 
+                    shadow_offset: int=0, target: Callable = None, args: Iterable=[]): 
                     """
                     Creates an NButton (New Button) object
 
@@ -30,6 +30,7 @@ class NButton:
                         - text_color = text color in all button states (default = (255, 255, 255))
                         - shadow colors (shadow_rect_color, shadow_hovered_color, shadow_selected_color,
                                         shadow_disabled_color, shadow_toggled_color)
+                        - transition_speed = speed of color transition (default = 15)
                         - fontsize = button text size (default = (height / 1.7))
                         - fontstyle = font styled used in the text (default = Cookie Run Regular)
                         - shadow_offset = x-offset of the shadow relative to the button rect (default = height * 0.25)
@@ -60,6 +61,9 @@ class NButton:
                     self.target = target
                     self.args = args
 
+                    self.transition_speed = transition_speed
+                    self.color_idx = 0
+
                     if fontsize >= 0:
                         self.fontsize = int(self.height / 1.7)
                     else:
@@ -72,6 +76,7 @@ class NButton:
 
                     self.toggled = False
                     self.clicked = False
+                    self.prev_state = 'Normal'
 
                     # Dict for button states : [button color, bool, shadow color]
                     self.states = {
@@ -100,6 +105,12 @@ class NButton:
             if bool[1]:
                 states = [key for key in self.states.keys()]
                 return states[idx]
+
+    def get_prev_state(self) -> state:
+        """
+        Returns the previous button state (dict key)
+        """
+        return self.prev_state
 
     def get_rect(self) -> pygame.Rect:
         """
@@ -139,11 +150,23 @@ class NButton:
             if self.states['Toggled'][1] or self.states['Selected'][1] or self.states['Disabled'][1]:
                 return
 
+        # if the current state is different from the next state (the passed arg)
+        if self.get_state() != state:
+            self.color_idx = 0
+            
         for states in self.states.keys():
             if states == state:
                 self.states[states][1] = True
             else:
+                if self.states[states][1]:
+                    self.prev_state = states
                 self.states[states][1] = False
+
+    def set_speed(self, speed):
+        """
+        Sets the color transition's speed
+        """
+        self.transition_speed = speed
 
     def set_text(self, text: str):
         """
@@ -197,16 +220,34 @@ class NButton:
             self.dx, self.dy = pos
             self.btn_rect.update(self.dx, self.dy, self.width, self.height)
             self.btn_shadow_rect.update(self.dx, self.dy+self.shadow_offset, self.width, self.height)
-            self.text_rect = pygame.Rect((self.btn_rect.x + self.btn_rect.w//2 - self.text_surface.get_width()//2,
-                                            self.btn_rect.y + self.btn_rect.h//2 - self.text_surface.get_height()//2),
-                                            self.text_surface.get_size())
+            self.text_rect = pygame.Rect((self.btn_rect.x + self.btn_rect.w//2 - 
+                                self.text_surface.get_width()//2, self.btn_rect.y + 
+                                self.btn_rect.h//2 - self.text_surface.get_height()//2),
+                                self.text_surface.get_size())
 
         # moves the button if toggled
         if self.toggled:
             self.btn_rect.move_ip(0, self.shadow_offset/1.5)
             self.text_rect.move_ip(0, self.shadow_offset/1.5)
 
+        # gets the current and previous shadow and button colors
+        current_color = pygame.color.Color(self.states[self.get_state()][0])
+        prev_color = pygame.color.Color(self.states[self.get_prev_state()][0])
+        current_shadow_color = pygame.color.Color(self.states[self.get_state()][2])
+        prev_shadow_color = pygame.color.Color(self.states[self.get_prev_state()][2])
+
+        if self.color_idx < self.transition_speed:
+            self.color_idx +=1
+
+        # transitioning the color from previous to current
+        fade_color = [pc + ((cc - pc)/self.transition_speed)*self.color_idx 
+                        for cc, pc in zip(current_color, prev_color)]
+        fade_shadow_color = [psc + ((csc - psc)/self.transition_speed)*self.color_idx 
+                        for csc, psc in zip(current_shadow_color, prev_shadow_color)]
+        
         # draws the text, shadow rect, and the btn rect on the surface
-        pygame.draw.rect(self.surface, self.states[self.get_state()][2], self.btn_shadow_rect, border_radius=self.border_radius)
-        pygame.draw.rect(self.surface, self.states[self.get_state()][0], self.btn_rect, border_radius=self.border_radius)
+        pygame.draw.rect(self.surface, fade_shadow_color, 
+                            self.btn_shadow_rect, border_radius=self.border_radius)
+        pygame.draw.rect(self.surface, fade_color, self.btn_rect, 
+                            border_radius=self.border_radius)
         self.surface.blit(self.text_surface, self.text_rect)
